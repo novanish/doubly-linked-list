7a7f14663551316acfb4d00ec5d7dc8c691b57b3
novanish <anishchhetri196@gmail.com>
2024-06-18 19:30:07 +0545

    initial commit: started implementation of a doubly linked list in typescript

---
+++ DoublyLinkedList.test.ts
@@ -0,0 +1,135 @@
+import { expect, test } from "bun:test";
+import { DoublyLinkedList } from ".";
+
+test("push", () => {
+  const array = [1, 2, 3, 4];
+  const doubly = new DoublyLinkedList<number>();
+
+  doubly.push(...array);
+  expect(doubly.length).toBe(array.length);
+  expect(doubly.toArray()).toEqual(array);
+
+  array.reverse();
+  array.push(0);
+  doubly.reverse().push(0);
+  expect(doubly.toArray()).toEqual(array);
+});
+
+test("pop", () => {
+  const array = [1, 2, 3, 4];
+  const doubly = new DoublyLinkedList<number>();
+
+  doubly.push(...array);
+
+  expect(doubly.pop()).toBe(array.pop() as number);
+  expect(doubly.toArray()).toEqual(array);
+  expect(doubly.length).toBe(array.length);
+
+  doubly.reverse();
+  array.reverse();
+
+  expect(doubly.pop()).toBe(array.pop() as number);
+  expect(doubly.toArray()).toEqual(array);
+  expect(doubly.length).toBe(array.length);
+
+  expect(new DoublyLinkedList().pop()).toBeUndefined()
+});
+
+test("unshift", () => {
+  const array = [];
+  const doubly = new DoublyLinkedList<number>();
+
+  array.unshift(1, 2);
+  doubly.unshift(1, 2);
+  expect(doubly.toArray()).toEqual(array);
+  expect(doubly.length).toEqual(array.length);
+
+  array.reverse();
+  doubly.reverse();
+  expect(doubly.toArray()).toEqual(array);
+
+  array.unshift(-1, 0);
+  doubly.unshift(-1, 0);
+  expect(doubly.toArray()).toEqual(array);
+  expect(doubly.length).toEqual(array.length);
+
+  array.reverse();
+  doubly.reverse();
+  expect(doubly.toArray()).toEqual(array);
+});
+
+test("shift", () => {
+  const array = [1, 2, 3, 4];
+  const doubly = new DoublyLinkedList<number>();
+
+  doubly.push(...array);
+
+  expect(doubly.shift()).toBe(array.shift() as number);
+  expect(doubly.toArray()).toEqual(array);
+  expect(doubly.length).toBe(array.length);
+
+  doubly.reverse();
+  array.reverse();
+
+  expect(doubly.shift()).toBe(array.shift() as number);
+  expect(doubly.toArray()).toEqual(array);
+  expect(doubly.length).toBe(array.length);
+
+  expect(new DoublyLinkedList().shift()).toBeUndefined();
+});
+
+test("reverse", () => {
+  const array = [1, 2, 3, 4];
+  const doubly = new DoublyLinkedList<number>();
+
+  doubly.push(...array);
+  expect(doubly.reverse().toArray()).toEqual(array.toReversed());
+  expect(doubly.reverse().toArray()).toEqual(array);
+  expect(doubly.length).toBe(array.length);
+});
+
+test("at", () => {
+  const doubly = new DoublyLinkedList<number>();
+
+  expect(doubly.at(0)).toBeUndefined();
+
+  doubly.push(1, 2, 3, 4);
+  expect(doubly.at(0)).toBe(1);
+  expect(doubly.at(1)).toBe(2);
+  expect(doubly.at(2)).toBe(3);
+  expect(doubly.at(3)).toBe(4);
+  expect(doubly.at(4)).toBeUndefined();
+
+  doubly.reverse();
+  expect(doubly.at(0)).toBe(4);
+  expect(doubly.at(1)).toBe(3);
+  expect(doubly.at(-1)).toBe(1);
+  expect(doubly.at(-2)).toBe(2);
+  expect(doubly.at(-5)).toBeUndefined();
+});
+
+test("toString", () => {
+  const doubly = new DoublyLinkedList();
+
+  expect(doubly.toString()).toBe("null");
+
+  doubly.push(1);
+  expect(doubly.toString()).toBe("null<-1");
+
+  doubly.push(2);
+  expect(doubly.toString()).toBe("null<-1<->2->null");
+
+  doubly.push(3);
+  expect(doubly.toString()).toBe("null<-1<->2<->3->null");
+});
+
+test("length", () => {
+  const doubly = new DoublyLinkedList<number>();
+  expect(doubly.length).toBe(0);
+
+  doubly.push(1);
+  expect(doubly.length).toBe(1);
+
+  doubly.push(2, 3, 4, 5);
+  expect(doubly.length).toBe(5);
+});
---
+++ index.ts
@@ -0,0 +1,172 @@
+class Node<T> {
+  constructor(
+    public data: T,
+    public previous: Node<T> | null = null,
+    public next: Node<T> | null = null
+  ) {}
+}
+
+export class DoublyLinkedList<T> {
+  private head: Node<T> | null = null;
+  private tail: Node<T> | null = null;
+
+  private isReversed: boolean = false;
+
+  public length: number = 0;
+
+  private insertAtLast(data: T): this {
+    const node = new Node(data);
+    this.length += 1;
+
+    if (this.tail == null) {
+      this.head = node;
+      this.tail = this.head;
+      return this;
+    }
+
+    node.previous = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+
+    return this;
+  }
+
+  private insertAtFirst(data: T): this {
+    const node = new Node(data);
+    this.length += 1;
+
+    if (this.head == null) {
+      this.head = node;
+      this.tail = this.head;
+      return this;
+    }
+
+    node.next = this.head;
+    this.head.previous = node;
+    this.head = node;
+
+    return this;
+  }
+
+  private getActualIndex(index: number): number {
+    const positiveIndex = index < 0 ? this.length + index : index;
+
+    if (!this.isReversed) return positiveIndex;
+
+    return this.length - 1 - positiveIndex;
+  }
+
+  public push(...data: Array<T>): number {
+    const insert = this.isReversed
+      ? this.insertAtFirst.bind(this)
+      : this.insertAtLast.bind(this);
+
+    for (let i = 0; i < data.length; i++) {
+      insert(data[i]);
+    }
+
+    return this.length;
+  }
+
+  public removeFromLast(): T | undefined {
+    if (this.tail == null) return;
+
+    const node = this.tail;
+
+    this.length--;
+    if (this.length === 0) {
+      this.head = this.tail = null;
+    } else {
+      this.tail = node.previous;
+      this.tail!.next = null;
+    }
+
+    return node.data;
+  }
+  public pop(): T | undefined {
+    return this.isReversed ? this.removeFromFirst() : this.removeFromLast();
+  }
+
+  public unshift(...data: Array<T>): number {
+    const insert = this.isReversed
+      ? this.insertAtLast.bind(this)
+      : this.insertAtFirst.bind(this);
+
+    for (let i = data.length - 1; i !== -1; i--) {
+      insert(data[i]);
+    }
+
+    return this.length;
+  }
+
+  private removeFromFirst(): T | undefined {
+    if (this.head == null) return;
+
+    const node = this.head;
+
+    this.length--;
+    if (this.length === 0) {
+      this.head = this.tail = null;
+    } else {
+      this.head = node.next;
+      this.head!.previous = null;
+    }
+
+    return node.data;
+  }
+
+  public shift(): T | undefined {
+    return this.isReversed ? this.removeFromLast() : this.removeFromFirst();
+  }
+
+  private getNodeAt(index: number): Node<T> | null {
+    const actualIndex = this.getActualIndex(index);
+    if (actualIndex < 0 || actualIndex >= this.length) return null;
+
+    const midPoint = Math.floor(this.length / 2);
+    const shouldStartFromLast = midPoint < index;
+    const startFrom = shouldStartFromLast ? this.length - 1 : 0;
+    const changeCountBy = shouldStartFromLast ? -1 : 1;
+    const prop = shouldStartFromLast ? "previous" : "next";
+
+    let node = shouldStartFromLast ? this.tail : this.head;
+    if (!node) return null;
+
+    for (let i = startFrom; i != actualIndex; i += changeCountBy) {
+      node = node![prop];
+    }
+
+    return node;
+  }
+
+  public at(index: number): T | undefined {
+    return this.getNodeAt(index)?.data;
+  }
+
+  public toArray(): Array<T> {
+    const array: Array<T> = [];
+    let node = this.isReversed ? this.tail : this.head;
+
+    while (node != null) {
+      array.push(node.data);
+      node = node[this.isReversed ? "previous" : "next"];
+    }
+
+    return array;
+  }
+
+  public toString(): string {
+    const array = this.toArray();
+
+    if (array.length === 0) return "null";
+    if (array.length === 1) return "null<-" + array[0];
+
+    return "null<-" + array.join("<->") + "->null";
+  }
+
+  reverse(): this {
+    this.isReversed = !this.isReversed;
+
+    return this;
+  }
+}
